75f874279185abd9816bbd834c8746e9f7ea7471
Tommy Doyle <tdoyle@andrew.cmu.edu>
2013-02-17 17:40:31 -0500

    Finished the basic skeleton of the app

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 var express = require('express');
 var app = express();
 
+var fs = require('fs');
+app.use(express.bodyParser());
+
+//Routes to serve web pages
+app.get("/",function(request,response) {
+	response.sendfile("static/home.html");
+});
+
+app.get("/static/:staticFilename", function (request, response) {
+    response.sendfile("static/" + request.params.staticFilename);
+});
+
+//API Requests
+
 app.listen("3000");
 console.log("Listening at 3000");
---
+++ static/homeScript.js
@@ -0,0 +1,1 @@
+/* Javascript for the client home page */
