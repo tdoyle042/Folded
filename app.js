a011525513a03554efbfcf28a180fac41485d487
Tommy Doyle <tdoyle@andrew.cmu.edu>
2013-02-19 15:23:08 -0500

    Progress on image uploading

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ var express = require('express');
 var app = express();
 
 var fs = require('fs');
+var atob = require('atob');
 app.use(express.bodyParser());
+
+var image;
 
 //Routes to serve web pages
 app.get("/",function(request,response) {
@@ -11,6 +14,23 @@ app.get("/",function(request,response) {
 
 app.get("/static/:staticFilename", function (request, response) {
     response.sendfile("static/" + request.params.staticFilename);
+});
+
+app.post("/saveImage",function (request,response) {
+	console.log("Got request");
+
+	var myimage = new Buffer(request.body.image,'base64');
+	image = myimage;
+	fs.writeFile("image.jpeg",myimage,function(err){
+		if(err) throw err;
+		console.log("saved file!");
+		response.send({sucess:true});
+	});
+});
+
+app.get("/getImage",function(request,response) {
+	console.log("get image!");
+	response.send({"image": image,sucess:true});
 });
 
 //API Requests
--- static/homeScript.js
+++ static/homeScript.js
@@ -1,1 +1,45 @@
 /* Javascript for the client home page */
+
+
+$(window).ready(function(){
+	console.log("ready!");
+	var canvas = $("#myCanvas")[0];
+	var ctx = canvas.getContext("2d");
+	ctx.fillRect(10,10,50,50);
+})
+
+function saveCanvas() {
+	var canvas = $("#myCanvas")[0];
+	var ctx = canvas.getContext("2d");
+	canvasImage = canvas.toDataURL("image/jpeg");
+	
+	// $.ajax({
+	// 	type: "post",
+	// 	url: "/saveImage",
+	// 	data: {
+	// 		"poop": "dollar"
+	// 		// id: 0,
+	// 		// image: canvasImage
+	// 	},
+	// 	// processData:false,
+	// 	sucess: function(data) {
+	// 		console.log("Attempted to send data");
+	// 	},
+	// 	error: function (err) {
+	// 		console.log("fuck this", err);
+	// 	}
+	// });
+	
+	$.post("/saveImage", {id: 0, image: canvasImage}, function (data) {
+		console.log(data);
+	});
+
+	$.get("/getImage", function(data) {
+		console.log(data.image);
+	});
+
+
+	
+
+
+}
